fd0974421f582b0a7e032706b3fbd7d3211b3a53
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-15 12:25:28 -0500

    Update maven config microservice

--- pragma-config/src/main/java/com/pragma/infrastructure/config/PragmaConfigApplication.java
+++ pragma-config/src/main/java/com/pragma/infrastructure/config/PragmaConfigApplication.java
@@ -1,8 +1,10 @@
-package com.pragma;
+package com.pragma.infrastructure.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class PragmaConfigApplication {
 
