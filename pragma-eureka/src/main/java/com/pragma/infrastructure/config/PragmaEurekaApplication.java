368749e807476c1fe8ee49ea1714c1762d89511d
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-15 12:33:58 -0500

    Configuration eureka

--- pragma-eureka/src/main/java/com/pragma/infrastructure/config/PragmaEurekaApplication.java
+++ pragma-eureka/src/main/java/com/pragma/infrastructure/config/PragmaEurekaApplication.java
@@ -1,8 +1,10 @@
-package com.pragma;
+package com.pragma.infrastructure.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class PragmaEurekaApplication {
 
