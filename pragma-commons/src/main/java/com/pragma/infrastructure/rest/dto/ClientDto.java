4a0793260b404521a9fe184ddcbf78decd450bdb
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-17 08:52:18 -0500

    Configuration mapper commons

---
+++ pragma-commons/src/main/java/com/pragma/infrastructure/rest/dto/ClientDto.java
@@ -0,0 +1,16 @@
+package com.pragma.infrastructure.rest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClientDto<T> {
+	private Long id;
+	private String name;
+	private String subname;
+	private T type;
+	private Long document;
+	private Integer age;
+	private String cityBirth;
+}
