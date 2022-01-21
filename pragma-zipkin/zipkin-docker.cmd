fa7b92c000e9ee01d453dabf223871c3300d3919
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-21 07:27:48 -0500

    Localhost

--- pragma-zipkin/zipkin-docker.cmd
+++ pragma-zipkin/zipkin-docker.cmd
@@ -1,6 +1,6 @@
 @echo off
 set RABBIT_ADDRESSES=localhost:5672
 set STORAGE_TYPE=mysql
-set MYSQL_USER=root
-set MYSQL_PASS=
+set MYSQL_USER=pragma-zipkin
+set MYSQL_PASS=pragma-zipkin
 java -jar ./zipkin-server-2.23.16-exec.jar
